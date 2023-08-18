38d2558ca73d710f132257dc786e570a0947108c
UzbekCodeWizard <uzbekcodewizard@gmail.com>
2023-08-18 22:40:36 +0500

    So'ngi o'zgarishlar

--- fayl.c
+++ fayl.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
 printf("Something");
-
+puts("Hello");
 
     return 0;
 }
--- main.py
+++ main.py
@@ -2,3 +2,5 @@ def FUNCTION(a: int, b: int)->int:
     return a + b
 
 print(FUNCTION(10, 15))
+
+print(FUNCTION(1, 2))
